9aa7b0550a395ffa3f5199c942fe7df51f7e98a2
Aaron Pentz <apentz49@gmail.com>
2015-08-18 15:30:44 -0700

    all methods working

--- crud/api/urls.py
+++ crud/api/urls.py
@@ -1,1 +1,9 @@
+from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
+from api.views import DetailUpdateDelete
 
+urlpatterns = [
+    url(r'^movies/$', 'api.views.list_create_view'),
+    url(r'^movies/(?P<movie_id>[0-9]+)/',
+        csrf_exempt(DetailUpdateDelete.as_view()))
+]
